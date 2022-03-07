c64243c2ec01d7a50eaf5fffd4f9e50d3dae1800
Moira Clancy <70659975+Moira7@users.noreply.github.com>
2022-03-07 18:14:02 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var heroHealth = 100;
+var heroAttackPower = 2;
+var b1CoolDown = 0;
+var b2CoolDown = 0;
+var b3CoolDown = 0;
+var b4CoolDown = 0;
+
+var enemyHealth = 100;
+var enemyAttackPower = 2;
+var turnCounter = 0;
+
+/*attackButton.addEventListener("click",function(e){
+    
+    var health = document.getElementById("healthIdicatorA");
+    currentHealth = currentHealth - attackPower;
+    console.log(health);
+    console.log(currentHealth);
+    health.setAttribute("style", "width:" + currentHealth + "%");
+    
+});
+
+*/
